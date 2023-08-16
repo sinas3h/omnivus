dc8abd4b8ba3aae4a9fb0915c2182b175861e3df
sina s3h <sinas3h@gmail.com>
2023-08-16 17:20:19 +0330

    fix menu bug

--- public/menu.js
+++ public/menu.js
@@ -6,10 +6,10 @@ menuIcon.addEventListener("click", menuFunction);
 function menuFunction() {
   if (menuBar.classList.contains("left-[-900px]")) {
     menuBar.classList.remove("left-[-900px]");
-    menuBar.classList.add("left-[-10px]");
+    menuBar.classList.add("left-[-20px]");
     menuIcon.innerHTML = '<i class="fa-solid fa-xmark text-3xl text-blue-600"></i>';
   } else {
-    menuBar.classList.remove("left-[-10px]");
+    menuBar.classList.remove("left-[-20px]");
     menuBar.classList.add("left-[-900px]");
     menuIcon.innerHTML = '<i class="fa-solid fa-bars text-3xl text-blue-600"></i>';
   }
